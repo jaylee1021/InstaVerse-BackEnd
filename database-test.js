c4ab2aa9832efbcbea8bc402580ae7d9e231edaf
Jay Lee <randomdori@gmail.com>
2023-07-09 02:37:08 -0700

    feat: add functions to create users/post with faker

---
+++ database-test.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { createRandomUser, createRandomPost } = require('./utils');
+
+// const { User, Post } = require('./models');
+const User = require('./models/user');
+const Post = require('./models/post');
+
+mongoose.connect('mongodb://localhost/instaverse', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+// once the database opens
+db.once('open', () => {
+    console.log('Connected to MongoDB Database: Mongoose App at HOST: ', db.host, 'PORT: ', db.port);
+});
+
+// if there is a database error
+db.on('error', (err) => {
+    console.log(`Database error: `, err);
+});
+
+// create 50 users
+for (let i = 0; i < 50; i++) {
+    User.create(createRandomUser())
+        .then((user) => {
+            // console.log(i, user);
+        })
+        .catch((err) => {
+            console.log('error', err);
+        });
+}
+
+// create 100 posts
+for (let i = 0; i < 100; i++) {
+    Post.create(createRandomPost())
+        .then((post) => {
+            // console.log(i, post);
+        })
+        .catch((err) => {
+            console.log('error', err);
+        });
+}
---
+++ utils.js
@@ -0,0 +1,27 @@
+const { faker } = require('@faker-js/faker');
+
+function createRandomUser() {
+    let firstName = faker.person.firstName();
+    let lastName = faker.person.lastName();
+    const emailDomain = faker.internet.email().split('@')[1];
+    let email = `${firstName}.${lastName}@${emailDomain}`;
+    let fullName = `${firstName} ${lastName}`;
+    return {
+        fullName: fullName,
+        email: email,
+        username: faker.internet.userName()
+    };
+}
+
+function createRandomPost() {
+    return {
+        username: faker.internet.userName(),
+        caption: faker.lorem.sentence(),
+        photo: faker.image.url()
+    };
+}
+
+module.exports = {
+    createRandomUser,
+    createRandomPost
+};

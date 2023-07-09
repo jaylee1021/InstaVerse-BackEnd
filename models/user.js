55472f57bf314c8ab14bb662d362e6e4c8aad0bf
Jay Lee <randomdori@gmail.com>
2023-07-09 00:43:42 -0700

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// create user schema
+const userSchema = new mongoose.Schema({
+    fullName: String,
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: String,
+    pronouns: String,
+    profilePicture: String,
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Follow' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Follow' }],
+    bio: String,
+    gender: String
+}, { timestamps: true });
+
+// create user model
+const User = mongoose.model('User', userSchema);
+
+// export user model
+module.exports = User;

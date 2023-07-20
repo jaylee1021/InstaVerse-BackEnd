429f56b3c744c651db61eb463e08c48e1cff15ab
Jay Lee <randomdori@gmail.com>
2023-07-19 21:48:38 -0700

    chore: fix cors issue

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ cloudinary.config({
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(cors());
 app.use(passport.initialize());
 

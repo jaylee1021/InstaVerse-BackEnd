769eeff5e5a209774c091f93c719ba23181f6f0a
Jay Lee <randomdori@gmail.com>
2023-07-19 21:59:41 -0700

    chore: fix server issue

--- database-test.js
+++ database-test.js
@@ -8,7 +8,7 @@ const Post = require('./models/post');
 const Follower = require('./models/follower');
 const Following = require('./models/following');
 
-mongoose.connect('mongodb://localhost/instaverse', {
+mongoose.connect(`${process.env.MONGO_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const Follower = require('./follower');
 
 
 // connect to the database
-mongoose.connect('mongodb://localhost/instaverse', {
+mongoose.connect(`${process.env.MONGO_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
